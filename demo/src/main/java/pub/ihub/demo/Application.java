bc6146aa1b7f39832d0b5cbfd1b770d8fa90961a
Henry <henry.git@outlook.com>
2024-05-04 19:06:23 +0800

    feat: 提交demo应用

---
+++ demo/src/main/java/pub/ihub/demo/Application.java
@@ -0,0 +1,17 @@
+package pub.ihub.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liheng
+ * @since 2024/5/4
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
